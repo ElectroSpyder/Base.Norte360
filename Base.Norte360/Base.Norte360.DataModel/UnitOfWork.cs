165acadda73e81bb8442bb34b219fd2e9f3358cd
ElectroSpyder <prg@live.com.ar>
2017-10-17 00:18:46 -0300

    Creando Capas de Acceso a Datos y Logica e Implementando UnitOfWork y Repository Patern, solo a nivel inicial

---
+++ Base.Norte360/Base.Norte360.DataModel/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using Base.Norte360.Logica.IUnitOfWork;
+
+namespace Base.Norte360.DataModel
+{
+    /// <summary>
+    /// La clase UnitOfWork contiene métodos para comenzar, comprometer y relanzar una transacción, 
+    /// también expone una propiedad Session que devuelve la sesión NHibernate actual
+    /// asociada a la unidad de trabajo. Cada instancia de UnitOfWork contiene una sola sesión.
+    /// 
+    /// El constructor estático se utiliza para implementar el patrón Singleton para la fábrica de sesión NHibernate,
+    /// en C # los constructores estáticos se ejecutan solo una vez por dominio de aplicación y 
+    /// son seguros para hilos, lo que los hace ideales para implementar singletons ().
+    /// Singleton es un patron cuyo proposito es asegurar que solo haya una instancia de clase
+    /// 
+    /// </summary>
+
+    public class UnitOfWork : IUnitOfWork
+    {
+       // private static readonly ISessionFactory SessionFactory;
+        public void BeginTransaction()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rollback()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Base.Norte360/Base.Norte360.Logica/IUnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace Base.Norte360.Logica.IUnitOfWork
+{
+    /// <summary>
+    /// Define los metodos para realizar un inicio, un commit o un rollback de la transaccion
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
